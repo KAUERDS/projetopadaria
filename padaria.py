ec1ce9f7328cfba0a0344f667d23a00d77db36c0
MarcosAthirson <marcossilvaathyrson137@gmail.com>
2025-10-01 11:59:59 -0300

    mundei tudo

--- padaria.py
+++ padaria.py
@@ -1,101 +1,100 @@
-from flask import *
+from flask import*
 
 app = Flask(__name__)
 receitas = []
-ingredientes = []
 
 @app.route("/")
 def paginaprincipal():
     return render_template("paginaprincipal.html")
 
-@app.route('/login', methods=['POST','GET'])
+
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    if request.method == 'GET':
-        return render_template('login.html')
-    else:
+    if request.method == 'POST':
         senha = request.form.get('senha')
         if senha == 'kaue':
-            return render_template('funcionarios.html')
+            return render_template('logado.html', msg="", lista=receitas)
         else:
-            return render_template('/.html')
+            return render_template('login.html', erro='Senha incorreta!')
+    return render_template('login.html')
 
 
-@app.route('/funcionarios', methods=['POST','GET'])
+@app.route('/funcionarios', methods=['GET', 'POST'])
 def salvar_funcionario():
-    nome = request.form.get('nome')
-    cargo = request.form.get('cargo')
-    telefone = request.form.get('telefone')
+    if request.method == 'POST':
+        nome = request.form.get('nome')
+        cargo = request.form.get('cargo')
+        telefone = request.form.get('telefone')
 
-    arquivo = open('funcionarios.txt', 'a')
-    linha = f'{nome}-{cargo}-{telefone}\n'
-    arquivo.write(linha)
-    arquivo.close()
+        with open('funcionarios.txt', 'a') as arquivo:
+            arquivo.write(f'{nome}-{cargo}-{telefone}\n')
 
     return render_template('funcionarios.html')
 
-@app.route('/cardapio', methods=['POST','GET'])
-def salvar_cardapio():
-    produto = request.form.get('produto')
-    preco = request.form.get('preco')
 
-    arquivo = open('cardapio.txt', 'a')
-    linha = f'{produto}-{preco}\n'
-    arquivo.write(linha)
-    arquivo.close()
+@app.route('/cardapio', methods=['GET', 'POST'])
+def salvar_cardapio():
+    if request.method == 'POST':
+        produto = request.form.get('produto')
+        preco = request.form.get('preco')
+
+        with open('cardapio.txt', 'a') as arquivo:
+            arquivo.write(f'{produto}-{preco}\n')
 
     return render_template('cardapio.html')
 
 
-@app.route('/receitas', methods=['get'])
-def adicionar_receitas():
-    global receitas
-    nome = request.form.get('nome')
-    return render_template('receitas.html')
-
-@app.route('/adicionar', methods=['post','get'])
+@app.route('/adicionar', methods=['GET', 'POST'])
 def adicionar_receita():
-    if request.method == 'GET':
-        return render_template('adicionareceita.html')
+    print('cheguei')
     global receitas
-    nome = request.form.get('nome')
-    preco = request.form.get('preco')
-    ingredientes = request.form.get('ingredientes')
+    if request.method == 'POST':
+        nome = request.form.get('nome')
+        preco = request.form.get('preco')
+        ingredientes = request.form.get('ingredientes')
 
-    receitas.append([nome, preco, ingredientes])
-    mensagem = nome + ' Sua receita foi adicionado com sucesso'
-    return render_template('receitaslista.html', msg=mensagem, lista=receitas)
+        print(nome)
+        print( preco)
+        print(ingredientes)
 
-@app.route('/remover', methods=['post'])
+        receitas.append([nome, preco, ingredientes])
+        msg = f'{nome} foi adicionada com sucesso!'
+        return render_template('logado.html', msg=msg, lista=receitas)
+
+    return render_template('adicionareceita.html')
+
+
+@app.route('/remover', methods=['POST'])
 def remover_receita():
-    #torna a variável modificável no escopo global
     global receitas
     nome = request.form.get('nome')
-    if nome in receitas:
-        receitas.remove(nome)
-
+    for receita in receitas:
+        if receita[0] == nome:
+            receitas.remove(receita)
+            msg = f'{nome} removida com sucesso!'
+            break
     else:
-        msg = 'nao consta na lista de receitas'
+        msg = f'{nome} não foi encontrada na lista.'
 
-    return render_template('logado.html')
+    return render_template('logado.html', msg=msg, lista=receitas)
 
-@app.route('/receitaslistas', methods=['get'])
+
+@app.route('/receitaslista', methods=['GET'])
 def listar_receitas():
-    if len(receitas) > 0:
-        return render_template('receitaslista.html', lista=receitas, ingredientes=receitas)
-    else:
-        return render_template('listareceitas.html', ingredientes=receitas)
+    return render_template('receitaslista.html', lista=receitas)
 
-@app.route('/avaliacoes', methods=['POST','GET'])
+
+@app.route('/avaliacoes', methods=['GET', 'POST'])
 def salvar_avaliacao():
-    usuario = request.form.get('usuario')
-    comentario = request.form.get('comentario')
+    if request.method == 'POST':
+        usuario = request.form.get('usuario')
+        comentario = request.form.get('comentario')
 
-    arquivo = open('avaliacoes.txt', 'a')
-    linha = f'{usuario}-{comentario}\n'
-    arquivo.write(linha)
-    arquivo.close()
+        with open('avaliacoes.txt', 'a') as arquivo:
+            arquivo.write(f'{usuario}-{comentario}\n')
 
     return render_template('avaliacoes.html')
 
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
